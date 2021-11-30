3a2b01f6e0a070ec49389302f61cdf7f4c5c82f7
cdms39 <93769007+cdms39@users.noreply.github.com>
2021-11-30 15:06:03 +0800

    Updated game_master.py
    Added some more comments to describe the code

--- game_master.py
+++ game_master.py
@@ -5,6 +5,8 @@ import os
 class Game_Master():
 
     def __init__(self):
+        # Initializes the cards to be played
+        
         self.SPECIAL_CARDS = {
             "K": 10,
             "Q": 10,
@@ -18,12 +20,15 @@ class Game_Master():
         for key in self.SPECIAL_CARDS:
             self.cards.append(key)
 
+        # Initializes the game to begin
         self.play = 'y'
         self.game_over = False
         self.to_hit = 'y'
         
         
     def initialize_state(self):
+        """Re-initializes the game state when playing again"""
+        
         self.game_over = False
         self.to_hit = 'y'
 
@@ -39,6 +44,7 @@ class Game_Master():
 
     def calculate_hand(self, hand):
         """Adds the sum of the hand of cards"""
+        
         total_sum = 0
 
         for card in hand:
@@ -61,6 +67,7 @@ class Game_Master():
 
     def set_result(self, player_sum, dealer_sum):
         """Sets round result based on the sums"""
+        
         if player_sum <= 21 and dealer_sum <= 21:
             if player_sum > dealer_sum:
                 return "\nYou win!"
@@ -78,11 +85,14 @@ class Game_Master():
 
 
     def hit_card(self, hitter):
+        """Allows player to hit to their hand"""
+        
         while (self.to_hit != 'n' and hitter.hand_sum < 21) and self.game_over == False:
             self.to_hit = input("\nWould you like to hit ('y' or 'n')? ")
             self.to_hit = self.to_hit.lower()
 
             if self.to_hit == 'y':
+                # Adds a card if 'yes', and calculates the updated sum of the hand
                 hitter.add_card(self.cards[random.randint(0, len(self.cards)-1)])
                 hitter.hand_sum = self.calculate_hand(hitter.hand)
                 print(f"\nYour Hand: {hitter.hand}")
@@ -91,16 +101,22 @@ class Game_Master():
                 print("Choice invalid.")
         
             if hitter.hand_sum > 21:
+                #Sets game over if player's hand is now over 21
                 self.game_over = True
 
 
     def set_dealer(self, dealer):
+        """Adds more cards to the dealer's hand"""
+        
+        #Adds more cards to the dealer's hand, as long as their total is still less than 17
         while dealer.hand_sum < 17:
             dealer.hand.append(self.cards[random.randint(0, len(self.cards)-1)])
             dealer.hand_sum = self.calculate_hand(dealer.hand)
 
 
     def to_play_again(self):
+        """Asks player if they'd like to play again"""
+        
         self.play = 'n'
 
         while self.play != 'y':
@@ -113,5 +129,3 @@ class Game_Master():
             elif self.play != 'n':
                 print("\nChoice invalid.")
 
-
-    
